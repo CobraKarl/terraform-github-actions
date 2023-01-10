65215b1dbaddc9f4253c7599761abaaf4dbe8ca3
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-10 09:07:20 +0100

    ffmt on main.tf

--- main.tf
+++ main.tf
@@ -11,41 +11,41 @@ terraform {
     storage_account_name = "storageforstate0822"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    
+
   }
 }
 
 provider "azurerm" {
-    features {
-      
-    }
-  
+  features {
+
+  }
+
 }
 
 data "azurerm_client_config" "current" {
-  
+
 }
 
 # Create RG
 resource "azurerm_resource_group" "mkk16" {
-    name = "mkk16"
-    location = "northeurope" 
+  name     = "mkk16"
+  location = "northeurope"
 }
 
 # Create VNET
 resource "azurerm_virtual_network" "vnet" {
-    name = "vnet"
-    resource_group_name = azurerm_resource_group.mkk16.name
-    location = azurerm_resource_group.mkk16.location
-    address_space = [ "10.0.0.0/16" ]
-  
+  name                = "vnet"
+  resource_group_name = azurerm_resource_group.mkk16.name
+  location            = azurerm_resource_group.mkk16.location
+  address_space       = ["10.0.0.0/16"]
+
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet1" {
-    name = "subnet1"
-    resource_group_name = azurerm_resource_group.mkk16.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = [ "10.0.1.0/24" ]
-  
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.mkk16.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
 }
